d59c6eee7a95deaa2720fce89313a25f82b89cee
mhastin <mhastiningrat@gmail.com>
2024-05-09 22:52:04 +0700

    dirname only

--- controllers/summary.js
+++ controllers/summary.js
@@ -2344,7 +2344,7 @@ const exportExcelPage2 = async (req, res) => {
 		let datePrint = `${moment().format("YYYYMMDD")}_${moment().format(
 			"HHmmss"
 		)}`;
-		const direct = path.join(__dirname, "");
+		const direct = __dirname;
 		await wb.xlsx
 			.writeFile(
 				`${direct}/COAL_TERMINAL_SUMMARY_REPORT_12_HOURS_SHIFT_PAGE_2_${datePrint}.xlsx`
